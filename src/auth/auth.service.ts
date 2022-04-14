323da9a57d554ca14106dc341cbb3663108a0612
denis <deshchenko@zuzex.com>
2022-04-14 18:01:54 +0300

    delete any type

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(username: string, password: string): Promise<any> {
+  async validateUser(username: string, password: string) {
     const user = await this.usersService.findOne(username);
     if (user && user.password === password) {
       const { password, ...result } = user;
